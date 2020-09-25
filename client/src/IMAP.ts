c10766fb3c931d658ed5b8bc964bec3bfbfce820
caueguedes <caue.guedes91@gmail.com>
2020-09-25 19:38:13 -0300

    client IMAP worker

---
+++ client/src/IMAP.ts
@@ -0,0 +1,37 @@
+import axios, { AxiosResponse } from "axios";
+
+import { config } from "./config";
+
+export interface IMailbox { name: string, path: string }
+
+export interface IMessage { 
+  id: string, date: string, from: string,
+  subject: string, body?: string
+}
+
+export class Worker {
+  public async listMailboxes(): Promise<IMailbox[]> {
+    const response: AxiosResponse =
+      await axios.get(`${config.serverAddress}/mailboxes`);
+    return response.data;
+  }
+
+  public async listMessages(inMailbox: string): Promise<IMessage[]> {
+    const response: AxiosResponse = await axios.get(
+      `${config.serverAddress}/mailboxes/${inMailbox}`
+    );
+    return response.data;
+  }
+
+  public async getMessageBody(inID: string, inMailbox: String): Promise<string> {
+    const response: AxiosResponse = await axios.get(`${config.serverAddress}/messages/${inMailbox}/${inID}`);
+    return response.data;
+
+  }
+
+  public async deleteMessage(inID: string, inMailbox: String): Promise<void> {
+    await axios.delete(
+      `${config.serverAddress}/messages/${inMailbox}/${inID}`
+    );
+  }
+}
