61970ca4dae919f55b588e75615b4f61a6012fba
caueguedes <caue.guedes91@gmail.com>
2020-09-25 19:39:28 -0300

    client Contacts Worker

---
+++ client/src/Contacts.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosResponse } from "axios";
+
+import { config } from "./config";
+
+export interface IContact {
+  _id?: number, name: string,  email: string
+}
+
+export class Worker {
+  
+  public async addContact(inContact: IContact): Promise<IContact> { 
+    const response: AxiosResponse = await axios.post(
+      `${config.serverAddress}/contacts`, inContact
+    );
+    return response.data;
+  }
+
+  public async deleteContact(inID: number): Promise<void> {
+    await axios.delete(
+      `${config.serverAddress}/contacts/${inID}`
+    );
+  }
+}
