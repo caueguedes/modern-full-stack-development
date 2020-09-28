5f4875b2c2305620448a7914a00c820156d2f7b6
caueguedes <caue.guedes91@gmail.com>
2020-09-28 00:34:03 -0300

    Moving files to chapter 9

--- client/src/config.ts
+++
@@ -1,6 +0,0 @@
-export const config: { serverAddress: string, userEmail: string } =
-{ 
-  serverAddress : "http://localhost", 
-  userEmail : "caue.guedes91@gmail.com" 
-};
-
--- server/dist/ServerInfo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverInfo = void 0;
-var path = require("path");
-var fs = require("fs");
-var rawInfo = fs.readFileSync(path.join(__dirname, "../serverInfo.json"));
-exports.serverInfo = JSON.parse(rawInfo);
-console.log("ServerInfo: ", exports.serverInfo);
-//# sourceMappingURL=ServerInfo.js.map
