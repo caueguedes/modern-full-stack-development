f90fd4030d24a85b71b8d460af23544e933497bf
caueguedes <caue.guedes91@gmail.com>
2020-10-05 20:04:02 -0300

    chapter 6 - battlejong server

---
+++ battle_jong/server/src/server.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import express, { Express } from "express";
+
+import WebSocket from "ws";
+
+// { score: number, stillPlaying: boolean }
+const players: any = {};
+
+const app: Express = express();
+app.use("/", express.static(path.join(__dirname, "../../client/dist")));
+app.listen(80, () => {
+  console.log("BattleJong Express server ready.")
+});
+
+
+const wsServer = new WebSocket.Server({ port: 8080 }, function() {
+  console.log("BattleJong WebSocket server ready.")
+});
+
+wsServer.on("connection", (socket: WebSocket) => {
+  socket.on("message", (inMsg: string) => {
+    const msgParts: string[] = inMsg.toString().split("_");
+    const message: string = msgParts[0];
+    const pid: string = msgParts[1];
+    switch(message) {
+      
+    }
+  });
+});
