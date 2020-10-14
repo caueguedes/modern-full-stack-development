2cda2a6a4a3a8604d7042fcddb0820fa447f72e9
caueguedes <caue.guedes91@gmail.com>
2020-10-13 21:07:47 -0300

    chapter 10 - battlejong - checking result case - done

--- battle_jong/server/src/server.ts
+++ battle_jong/server/src/server.ts
@@ -2,6 +2,7 @@ import path from "path";
 import express, { Express } from "express";
 
 import WebSocket from "ws";
+import { WSANOTINITIALISED } from "constants";
 
 // { score: number, stillPlaying: boolean }
 const players: any = {};
@@ -25,12 +26,42 @@ wsServer.on("connection", (socket: WebSocket) => {
     switch(message) {
       case "match":
         players[pid].score += parseInt(msgParts[2]);
-        wsServer.clients.forEach(
-          function each(inClient: WebSocket) {
+        wsServer.clients.forEach(function each(inClient: WebSocket) {
             inClient.send(`update_${pid}_${players[pid].score}`);
           }
         );
       break;
+
+      case "done":
+        players[pid].stillPlaying = false;
+        let playersDone: number = 0;
+        for (const player in players) {
+          if(players.hasOwnProperty(player)) {
+            if(!players[player].stillPlaying){
+              playersDone++;
+            }
+          }
+        }
+        // Checking end of game
+        if (playersDone === 2) {
+          let winningPID: string;
+          const pids: string[] = Object.keys(players);
+        
+          if (players[pids[0]].score > players[pids[1]]) {
+            winningPID = pids[0];
+          } else {
+            winningPID = pids[1];
+          }
+
+          wsServer.clients.forEach(
+            function each(inClient: WebSocket) {
+              inClient.send(`gameOver_${winningPID}`)
+            }
+          );
+        } 
+      break;
+
+
     }
   });
 });
